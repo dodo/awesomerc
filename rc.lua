12d6b026b26da57d14bb44cbb94b5eecee10ae85
dodo <dodo@blacksec.org>
2011-02-09 10:44:44 +0100

    first blush

--- rc.lua
+++ rc.lua
@@ -477,7 +477,7 @@ client.connect_signal("manage", function (c, startup)
     end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("focus",   function(c) c.border_color = beautiful.border_focus  end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 require("autostart")
