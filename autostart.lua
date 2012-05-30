35aaa0551a148626c6b39518aa542c1881f7058c
dodo <dodo.the.last@gmail.com>
2012-05-30 15:48:05 +0200

    Another commit to keep my CAN streak going.

--- autostart.lua
+++ autostart.lua
@@ -1,7 +1,7 @@
 local _ = require("runonce")
 
-_.run "unagi" -- http://unagi.mini-dweeb.org/
-_.run "nm-applet"
+-- _.run "unagi" -- http://unagi.mini-dweeb.org/
+-- _.run "nm-applet"
 -- _.run "kmix"
 
 
