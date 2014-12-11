e76ce4abf9fde203b97115a7fda05535f46e63cd
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-12-11 02:29:30 +0100

    add awful.client.movetoscreen fix?

--- rc.lua
+++ rc.lua
@@ -15,6 +15,20 @@ keydoc = require("keydoc")
 repl = require("uzful.widget.repl")
 
 
+awful.client.movetoscreen = function (c, s)
+    local _screen = require("awful.screen")
+    local sel = c or client.focus
+    if sel then
+        local sc = screen.count()
+        if not s then
+            s = sel.screen + 1
+        end
+        if s > sc then s = 1 elseif s < 1 then s = sc end
+        sel.screen = s
+        _screen.focus(s)
+        if sel.screen ~= s then sel.screen = s end
+    end
+end
 
 
 -- default values for notifications
