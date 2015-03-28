9496c5f011c635548f45d99fa03c4f6ec14ec15a
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2015-03-28 05:22:02 +0100

    really ignore ignored worsd

--- autostart.lua
+++ autostart.lua
@@ -1,7 +1,10 @@
 local run = require("runonce").run
 local spawn = require("awful.util").spawn
 
-spawn "dbus-send --session --print-reply --dest='org.kde.kded' /kded org.kde.kded.loadModule string:kdeconnect"
+if rc.conf.dbus and rc.conf.phone then
+    spawn "kdeinit4" -- start kded dbus service
+    spawn "dbus-send --session --print-reply --dest='org.kde.kded' /kded org.kde.kded.loadModule string:kdeconnect"
+end
 
 -- run "unagi" -- http://unagi.mini-dweeb.org/
 -- run "nm-applet"
